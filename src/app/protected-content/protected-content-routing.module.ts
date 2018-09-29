0f0e7930c7f5ba8428436fe22a2b9ef6d57b939e
Brian Kopp <briankopp.usa@gmail.com>
2018-09-29 17:39:52 -0500

    Adding protected content module

---
+++ src/app/protected-content/protected-content-routing.module.ts
@@ -0,0 +1,14 @@
+
+import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { MainComponent } from './components/main/main.component';
+
+export const routes: Routes = [
+  { path: '', component: MainComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ProtectedContentRoutingModule {}
