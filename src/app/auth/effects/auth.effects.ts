11f2e06aa52cb9d0102e1b1ccb6418e8f84fcef0
Brian Kopp <briankopp.usa@gmail.com>
2018-10-01 18:37:13 -0500

    fixed infinite loop

--- src/app/auth/effects/auth.effects.ts
+++ src/app/auth/effects/auth.effects.ts
@@ -1,11 +1,23 @@
 import { Injectable } from "@angular/core";
-import { Actions } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
 import { CognitoService } from '../services/cognito.service';
+import { LOGIN_USER_SUCCESS, LoginUserSuccessAction } from "../actions/auth.actions";
+import { Router } from "@angular/router";
+import { tap, map } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { Action } from "@ngrx/store";
 
 @Injectable()
 export class AuthorizationEffects {
     constructor(
       private actions: Actions,
-      private cognitoService: CognitoService
+      private cognitoService: CognitoService,
+      private router: Router
     ) { }
+
+    @Effect({dispatch: false})
+    loginSuccess$ = this.actions.pipe(
+      ofType(LOGIN_USER_SUCCESS),
+      tap(_ => this.router.navigate(['/']))
+    );
 }
