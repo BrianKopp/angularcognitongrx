895f100b26e1e6f891a24c4b51d079ddc8694cef
Brian Kopp <briankopp.usa@gmail.com>
2018-09-30 19:21:26 -0500

    Beginning to build out register form

--- src/app/auth/components/register-form/register-form.component.ts
+++ src/app/auth/components/register-form/register-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RegisterFormData } from '../../models/registerformdata';
 
 @Component({
   selector: 'app-register-form',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
+  @Input() errorMessage: string | null;
+  @Output() submitted = new EventEmitter<RegisterFormData>();
+
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    passwordConfirm: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl('')
+  });
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  submit() {
+    if (this.form.valid) {
+      this.submitted.emit(this.form.value);
+    }
+  }
 }
