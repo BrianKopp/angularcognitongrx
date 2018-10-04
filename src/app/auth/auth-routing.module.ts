9fbee482e09c662281fbc786c5e63cc0f4e41f53
Brian Kopp <briankopp.usa@gmail.com>
2018-10-03 23:35:30 -0500

    Can't go to /login /signup if signed in

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginPageComponent } from './containers/login-page/login-page.component';
 import { RegisterPageComponent } from './containers/register-page/register-page.component';
+import { LoggedInGuard } from './services/loggedin-guard.service';
 
 const routes: Routes = [
-  { path: 'login', component: LoginPageComponent},
-  { path: 'signup', component: RegisterPageComponent}
+  { path: 'login', component: LoginPageComponent, canActivate: [LoggedInGuard]},
+  { path: 'signup', component: RegisterPageComponent, canActivate: [LoggedInGuard]}
 ]
 
 @NgModule({
