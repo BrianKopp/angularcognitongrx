232d400b01e00bf3485b706bb0b27fcc53cc010d
Brian Kopp <briankopp.usa@gmail.com>
2018-12-08 22:49:09 -0700

    configuring cognito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EffectsModule } from '@ngrx/effects';
@@ -15,7 +15,8 @@ import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { reducers, metaReducers } from './reducers';
-import { environment } from '../environments/environment.prod';
+import * as prodEnv from '../environments/environment.prod';
+import * as devEnv from '../environments/environment';
 
 @NgModule({
   imports: [
@@ -23,17 +24,21 @@ import { environment } from '../environments/environment.prod';
     BrowserModule,
     BrowserAnimationsModule,
     CoreModule,
-    AuthModule,
+    AuthModule.forRoot(
+      isDevMode
+        ? { cognitoAppClientId: devEnv.environment.cognitoAppClientId, cognitoUserPoolId: devEnv.environment.cognitoUserPoolId }
+        : { cognitoAppClientId: prodEnv.environment.cognitoAppClientId, cognitoUserPoolId: prodEnv.environment.cognitoUserPoolId }
+    ),
     AppRoutingModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([]),
     StoreRouterConnectingModule.forRoot(),
     StoreDevtoolsModule.instrument({
       name: 'Cognito App',
-      logOnly: environment.production,
-    }),
+      logOnly: prodEnv.environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
